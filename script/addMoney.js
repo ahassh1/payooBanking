4c40a17d712bf7ed0823ab9247fd9e8ff57e50f1
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-03-08 17:03:59 +0600

    small change

--- script/addMoney.js
+++ script/addMoney.js
@@ -1,22 +1,42 @@
+
 document.getElementById('add-money')
 .addEventListener('click', function(event){
     event.preventDefault();
 
-    const amount = document.getElementById("amount").value ;
-    const convertedAmount = parseFloat(amount);
-
+    
     const pin =document.getElementById('pin').value;
     const convertedPin = parseInt(pin);
 
+    const amount = document.getElementById("amount").value ;
+    const convertedAmount = parseFloat(amount);
+
+        
    const mainBalence = document.getElementById
    ('main-balence').innerText;
    const  convertedMainBalence = parseFloat(mainBalence);
 
-    if(convertedPin===1234){
-       const sum = convertedMainBalence + convertedAmount;
-       document.getElementById('main-balence').innerText= sum;
+
+    if(amount && pin){
+        if(convertedPin ===1234){
+            const sum = convertedMainBalence + convertedAmount;
+            document.getElementById("main-balence").innerText=sum;
+        }
+    
+       else{
+        alert('enter valid pin')  
+       }
     }
     else{
-        console.log('pin is not valid')
+        alert('enter amount')
     }
-})
+})  
+
+
+//     if(convertedPin===1234){
+//        const sum = convertedMainBalence + convertedAmount;
+//        document.getElementById('main-balence').innerText= sum;
+//     }
+//     else{
+//         console.log('pin is not valid')
+//     }
+// })
