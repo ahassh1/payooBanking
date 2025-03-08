ec9dc07695a9604646f1a2dd6c8f14e160da4fad
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-03-08 12:54:42 +0600

    handle login and redirect to the home page]

--- script/login.js
+++ script/login.js
@@ -1,4 +1,20 @@
 document.getElementById('login-btn')
-.addEventListener('click', function(){
-    console.log('hello')
-})
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const accoutNumber = document.getElementById('accout-number').value;
+
+        const pin = document.getElementById('pin').value;
+        const convartedPin = parseInt(pin)
+        // console.log(accoutNumber, pinNumber);
+        if(accoutNumber.length ===11){
+            if(convartedPin===1234){
+              window.location.href= './main.html'
+            }
+            else{
+                 alert('pin thik nai');
+            }
+        }
+        else{
+                alert('need to valid account number')
+        }
+    })
