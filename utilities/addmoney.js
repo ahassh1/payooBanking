dfa7a222e2b81333919bd06fccd2185193805314
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-03-09 13:36:20 +0600

    use shared function to get input value

--- utilities/addmoney.js
+++ utilities/addmoney.js
@@ -3,11 +3,15 @@ document.getElementById('add-money')
     event.preventDefault();
     const amount = getInputValueById('amount');
     const pin = getInputValueById('pin')
-    const account = document.getElementById('account').value;
+    const account = document.getElementById('accout-number').value;
+    const mainBalence = getInnerTextByID('main-balence');
+    console.log(mainBalence)
     
     if(account.length ===11){
         if(pin===1234){
-
+              const sum = mainBalence + amount;
+            //   document.getElementById('main-balence').innerText=sum;
+              setInnerTextByIDandValue('main-balence',sum)
         }
         else{
             console.log('pin not valid')
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -3,3 +3,14 @@ function getInputValueById(id){
     const convertedValue = parseFloat(value);
     return convertedValue;
 }
+
+function getInnerTextByID(id){
+    const value = document.getElementById(id).innerText;
+    const convertedValue = parseFloat(value)
+    const convertedValue;
+}
+
+function setInnerTextByIDandValue(id, value){
+    // console.log(id, value)
+    document.getElementById(id).innerText = value;
+}
